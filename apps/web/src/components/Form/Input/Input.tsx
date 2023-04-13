a5fcd5abe9caacf9cfd7e7b39fc5c92c341b3fb1
corey-recai <coreycrecai@gmail.com>
2023-04-13 16:56:41 -0400

    add login form

---
+++ apps/web/src/components/Form/Input/Input.tsx
@@ -0,0 +1,54 @@
+import React, { ChangeEventHandler, forwardRef, RefObject } from "react";
+
+interface Props {
+  name: string;
+  type: string;
+  title: string;
+  className: string;
+  placeholder?: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  autoComplete: string;
+  value?: string;
+  errorDiv?: string;
+  errorMsg?: string;
+}
+
+export const Input = forwardRef(
+  (
+    {
+      name,
+      type,
+      title,
+      className,
+      placeholder,
+      onChange,
+      autoComplete,
+      value,
+      errorDiv,
+      errorMsg,
+    }: Props,
+    ref: RefObject<HTMLInputElement>
+  ) => {
+    return (
+      <div className='mb-3'>
+        <label htmlFor={name} className='form-label'>
+          {title}
+        </label>
+        <input
+          type={type}
+          className={className}
+          id={name}
+          ref={ref}
+          name={name}
+          placeholder={placeholder}
+          onChange={onChange}
+          autoComplete={autoComplete}
+          value={value}
+        />
+        <div className={errorDiv}>{errorMsg}</div>
+      </div>
+    );
+  }
+);
+
+export default Input;
--- apps/web/src/components/Login/Login.tsx
+++ apps/web/src/components/Login/Login.tsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { RefObject, useRef } from "react";
+import { Input } from "../Form/Input/Input";
 
 export const Login = () => {
+  const emailRef: RefObject<HTMLInputElement> | null = useRef(null);
+  const passwordRef: RefObject<HTMLInputElement> | null = useRef(null);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(emailRef.current.value);
+    console.log(passwordRef.current.value);
+
+    if (emailRef.current.value === "admin@example.com") {
+      return;
+    }
+  };
   return (
     <>
-      <div className='text-center'>
+      <div className='col-md-6 offset-md-3'>
         <h2>Login</h2>
         <hr />
+        <form onSubmit={handleSubmit}>
+          <Input
+            title='Email Address'
+            type='email'
+            className='form-control'
+            name='email'
+            autoComplete='email-new'
+            ref={emailRef}
+            onChange={e => console.log(emailRef.current?.value)}
+          />
+
+          <Input
+            title='Password'
+            type='password'
+            className='form-control'
+            name='password'
+            autoComplete='password-new'
+            ref={passwordRef}
+            onChange={e => console.log(passwordRef.current?.value)}
+          />
+        </form>
       </div>
     </>
   );
