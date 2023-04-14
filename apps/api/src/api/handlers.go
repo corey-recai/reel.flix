032c034f76ba363d6f55cecc6e655fb8427f4019
corey-recai <coreycrecai@gmail.com>
2023-04-13 21:32:40 -0400

    creating dummy data for movies route

--- apps/api/src/api/handlers.go
+++ apps/api/src/api/handlers.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
+
+	"reel.flix-api/src/models"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
@@ -26,4 +29,45 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(out)
 
+}
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+	var movies []models.Movie
+
+	rd, _ := time.Parse("2006-01-02", "1994-01-01")
+
+	shawshank := models.Movie{
+		ID:          1,
+		Title:       "The Shawshank Redemption",
+		ReleaseDate: rd,
+		RunTime:     116,
+		MPAARating:  "PG-13",
+		Description: "The Shawshank Redemption",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	movies = append(movies, shawshank)
+
+	godfather := models.Movie{
+		ID:          2,
+		Title:       "The Godfather",
+		ReleaseDate: rd,
+		RunTime:     116,
+		MPAARating:  "PG-13",
+		Description: "The Godfather",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	movies = append(movies, godfather)
+
+	out, err := json.Marshal(movies)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
 }
